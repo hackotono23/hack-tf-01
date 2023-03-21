dfa0fa71ed85be822897b518308101bfe2e4ea80
hackotono23 <128504076+hackotono23@users.noreply.github.com>
2023-03-21 17:33:48 +0000

    carga de main raiz

---
+++ Solution/main.tf
@@ -0,0 +1,49 @@
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform"
+    storage_account_name = "hackterraform"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"    
+  }
+  required_providers {
+    azurerm = {
+      version = "~> 2.19"
+    }
+  }
+}
+
+
+
+provider "azurerm" {
+  features {}
+}
+
+//Generation of ramdom String App Service
+resource "random_string" "str" {
+  length  = 3
+  special = false
+  upper   = false
+  number  = false
+}
+//Declaration of Locals Variables.
+locals {
+
+  common_tags = {
+    environment = "${var.prefix}"
+    project     = "${var.project}"
+    Terraform   = "true"
+    Environment = "dev"
+    Owner       = "test-user"
+  }
+  extra_tags = {
+    network = "HackTest"
+    extra = "Terraform"
+  }
+}
+
+//RG
+module "RGroups" {
+  source = "./Modules/RGroups"
+  tupla_rgname_lc = var.tupla_rgname_lc
+}
--- Solution/variables.tf
+++ Solution/variables.tf
@@ -1,1 +1,4 @@
-RG tupla_rgname_lc = { RGEU2001 = { name = "rghack01", location = "eastus" } RGCU001 = { name = "rghack02", location = "westus" } }
+variable tupla_rgname_lc {
+    type = map(any)
+}
+
